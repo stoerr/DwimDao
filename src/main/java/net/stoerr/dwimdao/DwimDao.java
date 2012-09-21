8e87cb48c2b62493070b30777ebeada58ecde040
Hans-Peter Stoerr <http://www.stoerr.net/>
2012-09-21 13:03:52 +0200

    Refactoring from bjoern-thalheim

--- src/main/java/net/stoerr/dwimdao/DwimDao.java
+++ src/main/java/net/stoerr/dwimdao/DwimDao.java
@@ -6,6 +6,7 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Proxy;
 import java.lang.reflect.Type;
 import java.util.Collection;
+import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -29,7 +30,7 @@ public class DwimDao implements InvocationHandler {
 
 	private final JdbcOperations jdbc;
 
-	public DwimDao(DataSource datasource) {
+	public DwimDao(final DataSource datasource) {
 		jdbc = new JdbcTemplate(datasource);
 	}
 
@@ -43,8 +44,9 @@ public class DwimDao implements InvocationHandler {
 	 * @return an implementation for the dao
 	 */
 	@SuppressWarnings("unchecked")
-	public static <DAO> DAO make(Class<DAO> daoClass, DataSource datasource) {
-		DwimDao handler = new DwimDao(datasource);
+	public static <DAO> DAO make(final Class<DAO> daoClass,
+			final DataSource datasource) {
+		final DwimDao handler = new DwimDao(datasource);
 		return (DAO) Proxy.newProxyInstance(daoClass.getClassLoader(),
 				new Class[] { daoClass }, handler);
 	}
@@ -55,56 +57,69 @@ public class DwimDao implements InvocationHandler {
 	 * @see java.lang.reflect.InvocationHandler#invoke(java.lang.Object,
 	 * java.lang.reflect.Method, java.lang.Object[])
 	 */
-	public Object invoke(Object proxy, Method method, Object[] args)
-			throws Throwable {
-		if (method.getName().startsWith("findBy")) {
+	public Object invoke(final Object proxy, final Method method,
+			final Object[] args) throws Throwable {
+		if (method.getName().startsWith("findBy"))
 			return doFind(method, args);
-		}
 		throw new IllegalArgumentException("We don't understand method "
 				+ method);
 	}
 
 	/** Executes the finder, depending on its name and return type. */
-	private Object doFind(Method method, Object[] args) {
-		Type returnType = method.getGenericReturnType();
-		String argdescr = method.getName().substring("findBy".length());
-		if (returnType instanceof Class) {
-			Class<?> beanClass = (Class<?>) returnType;
-			String sql = createFinderSql(argdescr, beanClass);
-			ParameterizedBeanPropertyRowMapper<?> rowmapper = ParameterizedBeanPropertyRowMapper
+	private Object doFind(final Method method, final Object[] args) {
+		final Type returnType = method.getGenericReturnType();
+		final String argdescr = method.getName().substring("findBy".length());
+		if (returnType instanceof Class)
+			return handleBeanClassReturnType((Class<?>) returnType, args,
+					argdescr);
+		else if (returnType instanceof ParameterizedType)
+			return handleParameterizedReturnType(
+					(ParameterizedType) returnType, args, argdescr);
+		throw new IllegalArgumentException("We don't understand return type "
+				+ returnType);
+	}
+
+	private List<?> handleParameterizedReturnType(
+			final ParameterizedType returnType, final Object[] args,
+			final String argdescr) {
+		final Type rawType = returnType.getRawType();
+		final Type[] typeargs = returnType.getActualTypeArguments();
+		if (Collection.class.equals(rawType)) {
+			final Class<?> beanClass = (Class<?>) typeargs[0];
+			final String sql = createFinderSql(argdescr, beanClass);
+			final ParameterizedBeanPropertyRowMapper<?> rowMapper = ParameterizedBeanPropertyRowMapper
 					.newInstance(beanClass);
-			try {
-				return jdbc.queryForObject(sql, args, rowmapper);
-			} catch (EmptyResultDataAccessException e) {
-				return null;
-			}
-		} else if (returnType instanceof ParameterizedType) {
-			ParameterizedType returnPType = (ParameterizedType) returnType;
-			Type rawType = returnPType.getRawType();
-			Type[] typeargs = returnPType.getActualTypeArguments();
-			if (Collection.class.equals(rawType)) {
-				Class<?> beanClass = (Class<?>) typeargs[0];
-				String sql = createFinderSql(argdescr, beanClass);
-				ParameterizedBeanPropertyRowMapper<?> rowMapper = ParameterizedBeanPropertyRowMapper
-						.newInstance(beanClass);
-				return jdbc.query(sql, rowMapper, args);
-			}
+			return jdbc.query(sql, rowMapper, args);
 		}
 		throw new IllegalArgumentException("We don't understand return type "
 				+ returnType);
+	}
+
+	private Object handleBeanClassReturnType(final Class<?> beanClass,
+			final Object[] args, final String argdescr) {
+		try {
+			final String sql = createFinderSql(argdescr, beanClass);
+			final ParameterizedBeanPropertyRowMapper<?> rowmapper = ParameterizedBeanPropertyRowMapper
+					.newInstance(beanClass);
+			return jdbc.queryForObject(sql, args, rowmapper);
+		} catch (final EmptyResultDataAccessException e) {
+			// we handle not existent beans with null value instead of
+			// exceptions.
+			return null;
+		}
 	}
 
 	/**
 	 * Creates a select statement according to the names of arguments in
 	 * argdescr, separated by And.
 	 */
-	private String createFinderSql(String argdescr, Class<?> beanClass) {
-		String[] argnames = argdescr.split("And");
-		StringBuilder buf = new StringBuilder("select * from ");
+	private String createFinderSql(final String argdescr,
+			final Class<?> beanClass) {
+		final String[] argnames = argdescr.split("And");
+		final StringBuilder buf = new StringBuilder("select * from ");
 		buf.append(beanClass.getSimpleName()).append(" where 1=1");
-		for (String arg : argnames) {
+		for (final String arg : argnames)
 			buf.append(" and ").append(arg).append(" = ?");
-		}
 		return buf.toString();
 	}
 
